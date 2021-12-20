298bef387b2155cf110490f2c6d43c35ab5945a9
visha <vishalhim@yahoo.com>
2021-12-20 22:06:39 +0530

    first commit

---
+++ WebApplication_crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication_crud.Models;
+
+namespace WebApplication_crud.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication_crud.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ WebApplication_crud/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_crud.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public Joke()
+        {
+
+        }
+    }
+}
